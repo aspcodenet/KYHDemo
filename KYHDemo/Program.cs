2fed48d6cab95bfedf718f4f908c185734e4295b
Stefan Holmberg <stefan.holmberg@systementor.se>
2025-09-26 12:33:04 +0200

    Add project files.

---
+++ KYHDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace KYHDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://api.thingspeak.com/update?api_key=5GA2OBFSGWT3TKF3&field1=12&field2=4
+            // loopa forever
+            //      generate random number för field1 och field2
+            //       skicka till thingspeak
+            //     vänta 15 sekunder    
+
+            var thingSpeak = new Thingspeak();
+            thingSpeak.SendData(12, 4);
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ KYHDemo/Thingspeak.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KYHDemo
+{
+    internal class Thingspeak
+    {
+        //public void SendData(int i, int i1)
+        //{
+        //    throw new NotImplementedException();
+        //}
+        public void SendData(int temperature, int humidity)
+        {
+            // https://api.thingspeak.com/update?api_key=5GA2OBFSGWT3TKF3&field1=12&field2=4
+            throw new NotImplementedException();
+        }
+    }
+}
